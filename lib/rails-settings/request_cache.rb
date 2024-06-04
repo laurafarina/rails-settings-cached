d6f5b08688ee89edbd98e03ba85e0e8dea3fdb5f
Jose Gonzalvo <jag@logica2.com>
2024-06-04 10:52:26 +0200

    Replace class_methods by class << self

--- lib/rails-settings/request_cache.rb
+++ lib/rails-settings/request_cache.rb
@@ -2,7 +2,7 @@ module RailsSettings
   module RequestCacheGetter
     extend ActiveSupport::Concern
 
-    class_methods do
+    class << self
       def enable!
         Thread.current[:rails_settings_request_cache_enable] = true
       end
